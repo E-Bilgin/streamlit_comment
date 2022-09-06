a5219a293e3c4f8150e01742ab7b0c2bb9dc2982
E.Bilgin <46282905+E-Bilgin@users.noreply.github.com>
2022-09-06 20:55:24 +0200

    Add files via upload

---
+++ my_utils/constants.py
@@ -0,0 +1,6 @@
+SCOPE = "https://www.googleapis.com/auth/spreadsheets"
+SPREADSHEET_ID = "b9ee0c0e99932e39b75dffe2cbe7491f3e93270a"
+SHEET_NAME = "comment"
+GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
+COMMENT_TEMPLATE_MD = """{} - {}
+> {}"""
